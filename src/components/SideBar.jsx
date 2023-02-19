eb15d268fc4488011399bd21d7a11b5cf5f88af6
Rafael Machado <70187787+Rafael-Machado01@users.noreply.github.com>
2023-02-19 17:40:55 -0300

    Error Solution and SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,27 @@
-import styles from "./Sidebar.modules.css";
+import { PencilLine } from "phosphor-react";
+import styles from "./Sidebar.module.css";
 
 export function SideBar() {
   return (
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1555765093-698c20d82b2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=100"
+        src="https://images.unsplash.com/photo-1606228281437-dc226988dc3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://xesque.rocketseat.dev/users/avatar/profile-899a33e5-bc1f-4cbd-82df-b6403f0fb47a-1652482537324.jpg"
+        />
         <strong>Rafael Machado</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu Perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
       </footer>
     </aside>
   );
