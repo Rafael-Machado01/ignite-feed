9dc3805bcfcd1f2406ce7a7aa140969b4bb8785c
Rafael Machado <70187787+Rafael-Machado01@users.noreply.github.com>
2023-02-19 13:32:43 -0300

    Css Global and Header Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./global.css";
 export function App() {
-    return ()
+  return (
+    <>
+      <Header />
+      <Post
+        author="Rafael Machado"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium alias laborum odio dicta illum ipsam provident similique! Dolorum modi, impedit, repellendus maiores veritatis expedita quisquam totam amet explicabo dolorem enim?"
+      />
+      <Post
+        author="Ana Júlia Miranda"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium alias laborum odio dicta illum ipsam provident similique! Dolorum modi, impedit, repellendus maiores veritatis expedita quisquam totam amet explicabo dolorem enim?"
+      />
+    </>
+  );
 }
