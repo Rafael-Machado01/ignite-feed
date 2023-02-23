c0735ba0d727b0ececf8d89be2f3d52596721492
Rafael Machado <70187787+Rafael-Machado01@users.noreply.github.com>
2023-02-23 14:40:46 -0300

    update stystem likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
-import { ThumbsUp, Trash } from "phosphor-react";
+import { HandsClapping, ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
   return (
     <div className={styles.comment}>
@@ -34,9 +41,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
-            <ThumbsUp />
-            Aplaudir <span>32</span>
+          <button onClick={handleLikeComment}>
+            <HandsClapping />
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
